cf8d45a5d39bacc7dd2c2987b669cbfec6c26f0f
Vincent ROBERT <vincent.robert@platform.sh>
2020-10-21 17:12:42 +0200

    only use the platform command + no need to use a SSH private key

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,7 @@ FROM pjcdawkins/platformsh-cli:latest
 RUN mkdir -p /root/.ssh && \
     echo "StrictHostKeyChecking no" > /root/.ssh/config
 
+RUN echo "GatewayPorts yes" >> /root/.ssh/config
+
 COPY ./entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,9 @@
 set -e
 
 # Grab relationship port and host to use in SSH command.
-RELATIONSHIP_PORT=$(platform relationships -p $PLATFORM_PROJECT -e $PLATFORM_ENVIRONMENT -A $PLATFORM_APP -P ${PLATFORM_RELATIONSHIP_NAME}.0.port)
 RELATIONSHIP_HOST=$(platform relationships -p $PLATFORM_PROJECT -e $PLATFORM_ENVIRONMENT -A $PLATFORM_APP -P ${PLATFORM_RELATIONSHIP_NAME}.0.host)
 
 echo "Don't forget to expose port $RELATIONSHIP_PORT in your docker-compose file!"
 
-# Open SSH tunnel in foreground and print detailed logs.
-ssh -i $PATHSSHKEY -n -N -L \
-  0.0.0.0:$RELATIONSHIP_PORT:$RELATIONSHIP_HOST:$RELATIONSHIP_PORT \
-  $(platform ssh -p $PLATFORM_PROJECT -e $PLATFORM_ENVIRONMENT -A $PLATFORM_APP --pipe) \
-  -o ServerAliveInterval=180 \
-  -o LogLevel=VERBOSE
+# Open SSH tunnel in foreground
+platform tunnel:single -p $PLATFORM_PROJECT -e $PLATFORM_ENVIRONMENT -A $PLATFORM_APP -r ${PLATFORM_RELATIONSHIP_NAME} -y
